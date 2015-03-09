8d4857c508a568281c895cf75ed8a645e5db4577
unknown <alexandre.arsenault@polymtl.ca>
2015-03-09 13:09:48 -0400

    Fixe merge.

--- Orchestra/Orchestra/Motion/VCNConstants.h
+++ Orchestra/Orchestra/Motion/VCNConstants.h
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #ifndef VICUNA_CONSTANTS
 #define VICUNA_CONSTANTS
 
@@ -21,28 +21,5 @@ const VCNFloat kMaxFloat = DBL_MAX;
 // PI is a handy constant to carry around
 const VCNFloat VCNPI = 3.14159265358979323846;
 
-=======
-#ifndef VICUNA_CONSTANTS
-#define VICUNA_CONSTANTS
 
-#include <float.h>
-
-// Max values
-//const VCNUShort kMaxUShort = (VCNUShort)-1;
-const VCNFloat kMinFloat = DBL_MIN;
-const VCNFloat kMaxFloat = DBL_MAX;
-
-// Resource core ID
-//const VCNResID kInvalidResID = (VCNResID)-1;
-
-// Animation core ID
-//const VCNAnimID kInvalidAnimID = (VCNAnimID)-1;
-
-// Maximum number of texture coordinate sets that are supported
-//#define MAX_TEX_COORD_SETS 4
-
-// PI is a handy constant to carry around
-const VCNFloat VCNPI = 3.14159265358979323846;
-
->>>>>>> d02eaa7ea9173b0b8b3cf013213142d8f06f22fb
 #endif
